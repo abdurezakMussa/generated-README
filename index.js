a82e31e83b8ad26db9329c4149e2eefa66fc79af
AbduEth <abdurekmjob@gmail.com>
2020-04-11 20:53:15 -0400

    change the video

--- index.js
+++ index.js
@@ -101,5 +101,5 @@ function init() {
         });
     });
 }
-
+<iframe src="https://drive.google.com/file/d/1k8kmHNDCD2EY5_BrqddsgF3Z6yhOJLKh/preview" width="640" height="480"></iframe>
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,7 +32,7 @@ function generateMarkdown(data) {
 ## Questions
   - ${data.questions}
   ## Link
-  <iframe src="https://drive.google.com/file/d/1k8kmHNDCD2EY5_BrqddsgF3Z6yhOJLKh/preview" width="640" height="480"></iframe>
+  https://drive.google.com/file/d/1k8kmHNDCD2EY5_BrqddsgF3Z6yhOJLKh/preview
 `
 ;
 }
